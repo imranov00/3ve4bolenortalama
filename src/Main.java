bc6aa9a8f8e6ae10bd3571363f0ba914ac96f17a
İmranov <semtexpert@gmail.com>
2023-12-24 17:50:02 +0300

    verison1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+
+        int toplam = 0;
+        int dongu = 0;
+
+        for (int i = 0; i <= sayi; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                toplam += i;
+                dongu++;
+            }
+        }
+        int netice = toplam/dongu;
+        System.out.println(netice);
+
     }
 }
